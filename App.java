b2f31071cceb04025df70c3f2fb3523de336e830
Marcos Marthe <41122217+marcosmarthe@users.noreply.github.com>
2021-02-26 11:41:56 -0300

    first commit

---
+++ App.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.*;  
+
+
+
+
+private class Contato {
+    
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner (System.in);
+ 
+    String nome = input.nextLine();
+    String email = input.nextLine();
+
+
+    
+    }
+
+}
+
+
+
+class Agenda {
+    public static void main(String args[]){
+
+    Vector<String> contato = new Vector<String>();
+    contato.add(nome);
+    contato.add(email);
+
+    }
+}
